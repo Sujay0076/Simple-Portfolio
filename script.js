ea661fb25cc9c075ea99095c3a05a837450877d2
Sujay0076 <sujayy639@gmail.com>
2025-05-16 16:46:00 +0530

    Add files via upload
    This is My First Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const sujay = document.querySelector(".sujay");
+const menu = document.querySelector(".navbar-menu .menu-bar");
+
+sujay.addEventListener("click",()=> {
+    menu.classList.toggle("show")
+})
